ed959f6d1559a5004c754b9ba4dffa53cafc6a7c
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2022-05-12 19:49:36 +0700

    Memperbaharui Pencarian Fit Proper

--- plnsuccessor/src/pages/PencarianFitProper.jsx
+++ plnsuccessor/src/pages/PencarianFitProper.jsx
@@ -1,5 +1,7 @@
 import HeaderTanpaPencarian from "../components/Header/HeaderTanpaPencarian";
-
+import CekNIPLogo from "../assets/icons/CekNIPLogo";
+import DoneCekLogo from "../assets/icons/DoneCekLogo";
+import { InputBase, Button } from "@mui/material";
 
 const PencarianFitProper = () => {
     return (
@@ -8,6 +10,17 @@ const PencarianFitProper = () => {
             <div className="TitlePencarianFitProper">
                 <p className="TitleUtamaPencarianFitProper">Pencarian Fit Proper</p>
                 <p className="TitleSecondPencarianFitProper">Welcome To Pencarian Fit & Proper</p>
+            </div>
+            <div className="BoxSearch">
+                <div className="TitleBox">
+                    <CekNIPLogo />
+                    <p className="TitleBoxSearch">Pencarian NIP Peserta</p>
+                </div>
+                <InputBase className="Inputbase" />
+                <Button className="ButtonCekNIP" variant="outlined">
+                    <DoneCekLogo />
+                    Submit
+                </Button>
             </div>
         </div>
     );
