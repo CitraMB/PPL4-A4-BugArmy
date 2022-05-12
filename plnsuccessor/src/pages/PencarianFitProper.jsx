a2e3174752e3d404f9aa5c95b732b7642515ca70
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2022-05-12 18:52:09 +0700

    Menambahkan Title Pencarian Fit Proper

--- plnsuccessor/src/pages/PencarianFitProper.jsx
+++ plnsuccessor/src/pages/PencarianFitProper.jsx
@@ -3,8 +3,12 @@ import HeaderTanpaPencarian from "../components/Header/HeaderTanpaPencarian";
 
 const PencarianFitProper = () => {
     return (
-        <div>
+        <div className="PencarianFitProper">
             <HeaderTanpaPencarian />
+            <div className="TitlePencarianFitProper">
+                <p className="TitleUtamaPencarianFitProper">Pencarian Fit Proper</p>
+                <p className="TitleSecondPencarianFitProper">Welcome To Pencarian Fit & Proper</p>
+            </div>
         </div>
     );
 };
