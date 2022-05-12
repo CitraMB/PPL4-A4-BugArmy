cf04d41384cbb5f65edffadc8b9b94b094fbc8e1
AlvinAndrian <91009495+AlvinAndrian@users.noreply.github.com>
2022-05-12 08:05:32 +0700

    Memperbaiki Responsive Sidebar

--- plnsuccessor/src/components/Sidebar/Sidebar.jsx
+++ plnsuccessor/src/components/Sidebar/Sidebar.jsx
@@ -133,7 +133,7 @@ const Sidebar = ({ children }) => {
     return (
         <div className="main-container">
             <motion.div animate={{
-                width: isOpen ? "220px" : "54px",
+                width: isOpen ? "260px" : "54px",
                 transition: {
                     duration: 0.1,
                     type: "spring",
@@ -180,7 +180,7 @@ const Sidebar = ({ children }) => {
                     })}
                 </section>
             </motion.div>
-            <main>{children}</main>
+            <main className="main">{children}</main>
         </div >
     );
 };
