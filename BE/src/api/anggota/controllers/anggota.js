b6aac6fd6ad0234db8add1ce0a90321e06128adc
mh-taufik <muhammad.taufik.tif20@polban.ac.id>
2022-05-12 12:41:11 +0700

    remove BE folder

--- BE/src/api/anggota/controllers/anggota.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  anggota controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::anggota.anggota');
--- BE/src/api/peran/routes/peran.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * peran router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::peran.peran');
