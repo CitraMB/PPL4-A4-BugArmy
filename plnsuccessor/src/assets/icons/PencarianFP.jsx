19b11f116c5f8cd6fe8c72b380a0542e1c31ab38
mh-taufik <muhammad.taufik.tif20@polban.ac.id>
2022-05-19 11:42:04 +0700

    update pencarian fitproper

---
+++ plnsuccessor/src/assets/icons/PencarianFP.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+function PencarianFP() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="50"
+      height="50"
+      fill="none"
+      viewBox="0 0 50 50"
+    >
+      <rect
+        width="22.917"
+        height="27.083"
+        x="10.417"
+        y="6.25"
+        fill="#7E869E"
+        fillOpacity="0.25"
+        rx="2"
+      ></rect>
+      <path
+        stroke="#007BFF"
+        strokeLinecap="round"
+        strokeWidth="3"
+        d="M18.75 14.583h8.333M18.75 31.25H25M18.75 22.917h12.5M39.583 22.917V12.25c0-2.828 0-4.243-.878-5.121-.879-.879-2.293-.879-5.122-.879H16.417c-2.829 0-4.243 0-5.122.879-.878.878-.878 2.293-.878 5.121v25.5c0 2.828 0 4.243.878 5.121.88.879 2.293.879 5.122.879H25"
+      ></path>
+      <circle
+        cx="36.458"
+        cy="36.458"
+        r="5.208"
+        stroke="#007BFF"
+        strokeLinecap="round"
+        strokeWidth="3"
+      ></circle>
+      <path
+        stroke="#007BFF"
+        strokeLinecap="round"
+        strokeWidth="3"
+        d="M43.75 43.75l-3.125-3.125"
+      ></path>
+    </svg>
+  );
+}
+
+export default PencarianFP;
