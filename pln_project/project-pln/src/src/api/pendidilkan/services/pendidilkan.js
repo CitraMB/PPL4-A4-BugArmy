c43633e765ff480a89feb91f094128419e876b00
ginaanifahchoirunnisa <gina.anifah.tif20@polban.ac.id>
2022-06-05 23:15:22 +0700

    strapi pembaruan

---
+++ pln_project/project-pln/src/src/api/pendidilkan/services/pendidilkan.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * pendidilkan service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::pendidilkan.pendidilkan');
